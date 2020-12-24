cdc1712afba1b835be7a0a2cd1a595c407e99fc0
Nick Banken <Nick.banken@gmail.com>
2020-12-24 16:44:29 +0100

    removed junk files

--- hugo/static/js/script.js
+++ hugo/static/js/script.js
@@ -11,12 +11,6 @@ let main = document.querySelector('main');
 let viewport = document.getElementById('viewport');
 let insertView = document.querySelector('#insertData');
 
-
-
-
-
-
-
 max.addEventListener("click", ()=>{
 	max.style.display = "none";
 	min.style.display = "inline-block";
@@ -42,8 +36,6 @@ for(let i = 0; i < inputView.length; i++){
 }
 
 
-
-
 let maxIcon = document.getElementById("max");
 let minIcon = document.getElementById("min");
 let parent = document.getElementById("insertData");
@@ -65,3 +57,59 @@ document.getElementById("editorButton").addEventListener("click",()=>{
 	instructionsView.classList.add("instructions--max")
 	editorView.classList.add("editor--max")
 })
+
+
+
+//Navigation code
+
+let btn = document.querySelector('.dropdown__btn');
+
+btn.addEventListener('click',myFunction)
+
+function myFunction() {
+	document.getElementById("dropdownList").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+	if (!event.target.matches('.dropdown__btn')) {
+	  var dropdowns = document.getElementsByClassName("dropdown__list");
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  }
+
+//################## Arrow navigation ###################
+
+  let links = document.querySelectorAll(".dropdown__list ul > li a");
+
+	for(let i=0; i<links.length; i++){
+		// added + "index.html" because on the server they open the folder and read the file.
+		if(JSON.stringify(links[i].href+"index.html").includes(window.location.pathname)){
+			console.log(links[i].href , window.location.pathname)
+			setNavArrow(links,i-1,".prev")
+			setNavArrow(links,i+1,".next")
+			links[i].classList.add("current")
+			break;
+		}
+	}
+
+
+	function setNavArrow(arr,index,query){
+		let arrowArr = document.querySelectorAll(query);
+		if(!arr[index]){
+			arrowArr.forEach(el=>{
+				el.classList.add("disabled");
+			})
+		}else{
+			arrowArr.forEach(el=>{
+				el.href = arr[index];
+			})
+		}
+	}
