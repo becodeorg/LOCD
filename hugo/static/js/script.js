ad6e0dfd227e12300d5428f8d6a78d4fc2b752e5
Nick <nick.banken@gmail.com>
2021-01-19 16:25:02 +0100

    made fix for service worker

--- hugo/static/js/script.js
+++ hugo/static/js/script.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("../service-worker.js")
+      .register("/service-worker.js")
       .then((reg) => console.log("Service worker: registered"))
       .catch((err) => console.log(`Service Worker: error ${err}`));
   });
