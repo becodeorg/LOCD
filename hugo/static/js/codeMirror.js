91606d0e3037e8f34e21dc992af48566f07a2553
Nick <nick.banken@gmail.com>
2021-02-02 17:41:09 +0100

    finished tabs

--- hugo/static/js/codeMirror.js
+++ hugo/static/js/codeMirror.js
@@ -1,5 +1,5 @@
 let HTMLeditor = CodeMirror.fromTextArea(
-    document.getElementById('textareaCode'), {
+    document.getElementById('textareaCodeHTML'), {
       theme:"locd-dark",
 		  lineNumbers: true,
       mode: 'text/html',
@@ -20,24 +20,46 @@ let HTMLeditor = CodeMirror.fromTextArea(
   }
 })
 
-//  let CSSeditor = CodeMirror.fromTextArea(
-//     document.getElementById('textareaCodeCSS'), {
-//       theme:"locd-dark",
-// 		  lineNumbers: true,
-//       mode: 'css',
-//       htmlMode: true,
-//       lineWrapping: true,
-// 		  smartIndent: false,
-//       addModeClass: true,
-//       extraKeys: {
-//         'Tab': 'emmetExpandAbbreviation',
-//         'Esc': 'emmetResetAbbreviation',
-//         'Enter': 'emmetInsertLineBreak',
-//         "Ctrl": "autocomplete"
-//     },
-//     globalVars: true,
-//     emmet: {
-//       preview:['html'],
-//       markupStyle:'html'
-//   }
-// })
+ let CSSeditor = CodeMirror.fromTextArea(
+    document.getElementById('textareaCodeCSS'), {
+      theme:"locd-dark",
+		  lineNumbers: true,
+      mode: 'css',
+      htmlMode: true,
+      lineWrapping: true,
+		  smartIndent: false,
+      addModeClass: true,
+      extraKeys: {
+        'Tab': 'emmetExpandAbbreviation',
+        'Esc': 'emmetResetAbbreviation',
+        'Enter': 'emmetInsertLineBreak',
+        "Ctrl": "autocomplete"
+    },
+    globalVars: true,
+    emmet: {
+      preview:['html'],
+      markupStyle:'html'
+  }
+})
+
+let JSeditor = CodeMirror.fromTextArea(
+    document.getElementById('textareaCodeJS'), {
+      theme:"locd-dark",
+		  lineNumbers: true,
+      mode: 'text/javascript',
+      htmlMode: true,
+      lineWrapping: true,
+		  smartIndent: false,
+      addModeClass: true,
+      extraKeys: {
+        'Tab': 'emmetExpandAbbreviation',
+        'Esc': 'emmetResetAbbreviation',
+        'Enter': 'emmetInsertLineBreak',
+        "Ctrl": "autocomplete"
+    },
+    globalVars: true,
+    emmet: {
+      preview:['html'],
+      markupStyle:'html'
+  }
+})
